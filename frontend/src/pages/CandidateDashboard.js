9cfa2ca0337e916821dd1515edd30d29e5ff12a6
Nikhil H <nikhilhalladamani3@gmail.com>
2025-10-22 08:00:02 +0530

    useNavigate

--- frontend/src/pages/CandidateDashboard.js
+++ frontend/src/pages/CandidateDashboard.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import DashboardCard from "../components/DashboardCard";
 import { getAllJobs } from "../api/api";
 
